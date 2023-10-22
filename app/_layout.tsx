37d4e7e2e181ba8ba3859b0624418d53f3d192e9
Harsh Tuwar <tuwarharsh08@gmail.com>
2023-10-23 01:47:10 +0530

    feat: added custom fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,17 @@ import { Stack, useRouter } from 'expo-router'
 import { Button } from 'react-native';
 import { PaperProvider } from 'react-native-paper'
 import { UserProvider } from '../context/UserContext';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const StackLayout = () => {
 	const router = useRouter();
+	const [fontsLoaded, fontError] = useFonts({
+		'Bubblegum Sans': require('../assets/fonts/BubblegumSans-Regular.ttf'),
+		'Futura': require('../assets/fonts/futur.ttf')
+	});
 
 	return (
 		<UserProvider>
---
+++ styles/index.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export const general = StyleSheet.create({
+	font: {
+		fontFamily: 'Bubblegum Sans'
+	},
+	font2: {
+		fontFamily: 'Futura'
+	},
+	center: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
+
+export const helpers = StyleSheet.create({
+	mb30: {
+		marginBottom: 30
+	},
+	mt30: {
+		marginTop: 30
+	},
+	m5: {
+		margin: 5
+	}
+});
+
