7f9c6e56a26a34e1a0082a1dbfc408f9eea4efc4
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2023-10-23 01:47:38 +0530

    feat: added custom fonts (#5)

--- app/(app)/recent-scans.tsx
+++ app/(app)/recent-scans.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { general } from '../../styles';
 
 const RecentScans = () => {
   return (
-	<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-	  <Text>Recent Scan scans</Text>
+	<View style={{ ...general.center }}>
+	  <Text style={general.font2}>Recent Scan scans</Text>
 	</View>
   )
 }
