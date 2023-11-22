e60d0edce4c97b5764a2455bba17cdaab6a9d0a0
Harsh Tuwar <38749882+Harsh-Tuwar@users.noreply.github.com>
2023-11-22 22:51:24 +0530

    added functionality to capture not found products barcodes (#19)

--- constants/index.ts
+++ constants/index.ts
@@ -3,6 +3,6 @@ export const USDA_FOOD_CENTRAL_API_URL = 'https://api.nal.usda.gov/fdc/v1/foods/
 export const USDA_FOOD_CENTRAL_API_KE_PARAM = 'api_key=5APKkhqM92krVp3jIEuMhGnBKyuuxIC89okJmEA0';
 
 export const FB_USER_COLLECTION_STRING = 'users';
-
+export const NOT_FOUND_PRODUCTS_STRING = '404-products';
 
 export const SNACK_DURATION = 3000;
